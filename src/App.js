c136788eaf2b5ed107927b7dcbc9f82aeb44dc76
Bassel Madarati <basselmadarati@gmail.com>
2023-02-11 19:09:02 +0300

    update

--- src/App.js
+++ src/App.js
@@ -4,19 +4,53 @@ import Item from './components/Item';
 import AddItem from './components/AddItem';
 
 class App extends react.Component {
-  state = {
-    cart: [{ id: 1, name: "bassel" }, { id: 2, name: "ahmad" }],
-    total: 2
+
+  constructor() {
+    super();
+    this.state = {
+      cart: [{ id: 1, name: "bassel" }, { id: 2, name: "ahmad" }],
+      total: 2
+    }
+
   }
 
+
+
+  addCart = (name) => {
+    if (name === "") {
+      alert("empty");
+      return;
+    }
+    const it = this.state.cart;
+    let max = 0;
+    it.forEach(element => {
+      //console.log(element)
+      if (max < element.id) {
+        //console.log("max ", max, "ID ", element.id)
+        max = element.id
+        //console.log("max", max)
+      }
+    });
+    max += 1
+
+    //console.log("temp", it)
+    this.setState({ cart: [...this.state.cart, { id: max, name: name }] })
+    this.setState({ total: this.state.total + 1 })
+
+
+  }
+
+
   render() {
-    const ss = this.state.cart.map(itm => <Item itmId={itm.id} itmName={itm.name} />)
-    console.log(ss[0])
+    const ss = this.state.cart.map(itm => <Item key={itm.id} itmId={itm.id} itmName={itm.name} />)
+    console.log(ss)
     return (
       <div className="App" >
-        <AddItem />
+        <h1>Total Items: {this.state.total}</h1>
+        <AddItem addC={this.addCart} />
         <hr></hr>
         {ss}
+
       </div>
     )
 
--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,14 +1,36 @@
 import React from "react";
 class AddItem extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { name: "" };
+        this.addC = props.addC
+        this.inputChangedHandler = this.inputChangedHandler.bind(this);
+
+    }
+
+    inputChangedHandler = (e) => {
+        this.setState({ name: e.target.value })
+    }
+    add = (e) => {
+        e.preventDefault()
+        //console.log(this.state.name)
+        this.addC(this.state.name)
+        this.state.name = ""
+
+
+
+    }
     render() {
+        //console.log(this.state)
+
         return (
             <>
                 <h1>ADD NEW</h1>
-                <form>
+                <form onSubmit={this.add}>
                     <label for="newName">New: </label>
-                    <input id="newName" type='text' />
-                    <button>ADD</button>
+                    <input id="newName" type='text' onChange={this.inputChangedHandler} value={this.state.name} />
+                    <button >ADD</button>
                 </form>
             </>
 
